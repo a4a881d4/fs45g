9fdf8a044b6e51fd5f2b1c875fa2001ce1c7e2e8
a4a881d4 <a4a881d4@163.com>
2015-07-09 08:52:21 +0800

    dir in dir

--- fs45g/fs45g.py
+++ fs45g/fs45g.py
@@ -10,7 +10,7 @@ class fs45g:
 		self.cashe = root
 
 	def makefile(self,hashkey,rc):
-		if rc == 'root':
+		if rc == 'meta':
 			path = self.root
 		else:
 			path = self.cashe
@@ -22,22 +22,75 @@ class fs45g:
 		filename = path + '/' + hashkey
 		
 		return filename
+	
+	def _createMeta( self, path, UID, GID, mode ):
+		my = dict()
+		my['path']=path
+		my['UID']=UID
+		my['GID']=GID
+		my['mode']=tools.fmode(mode)
+		my['ctime']=time.time()
+		my['mtime']=time.time()
+		my['atime']=time.time()	
+		return my
 
 	def prepareDir( self, path, UID, GID, mode ):
-		mydir = dict()
-		mydir['path']=path
-		mydir['UID']=UID
-		mydir['GID']=GID
-		mydir['mode']=tools.fmode(mode)
-		mydir['ctime']=time.time()
-		mydir['mtime']=time.time()
-		mydir['atime']=time.time()
-		mydir['files']={}
-		return tools.hashpath(path),mydir
+		mydir = self._createMeta( path, UID, GID, mode )
+		fns = path.split('/')
+		if fns:
+			mydir['name'] = fns[-1]
+		else:
+			mydir['name'] = ''
+		mydir['namehash'] = tools.hashpath(path)
+		mydir['type'] = 'dir'
+		mydir['files'] = []
+		return mydir
 
-	def persistenceDir( self, hashkey, obj ):
-		path = self.makefile( hashkey, 'root' )
+	def persistenceDir( self, mydir ):
+		path = self.makefile( mydir['namehash'], 'meta' )
 		f = open(path,'w')
-		f.write(json.dumps(obj,indent=2))
+		f.write(json.dumps(mydir['files'],indent=2))
 		f.close()
+		mydir['type'] = 'dirinmeta'
+		mydir['files'] = []
+
+	def prepareFileInDir( self, fn, UID, GID, mode, mydir ):
+		myfile = self._createMeta( mydir['path']+'/'+fn, UID, GID, mode )
+		myfile['name'] = fn
+		myfile['namehash'] = tools.hashpath(myfile['path'])
+		myfile['type'] = 'file'
+		mydir['files'].append(myfile)
+
+	def prepareDirInDir( self, fn, UID, GID, mode, mydir ):
+		myfile = self._createMeta( mydir['path']+'/'+fn, UID, GID, mode )
+		myfile['name'] = fn
+		myfile['namehash'] = tools.hashpath(myfile['path'])
+		myfile['type'] = 'dir'
+		myfile['files'] = []
+		mydir['files'].append(myfile)
+
+	def loadDirByPathRecursion( self, path ):
+		metapath = self.makefile( tools.hashpath(path), 'meta' )
+		with open(filepath) as meta_file:    
+			mydir_files = json.load(meta_file)
+			for f in mydir_files:
+				if f['type'] == 'dirinmeta':
+					f['files'] = self.loadDirByPath(f['path'])
+					f['type'] = 'dir'
+			return mydir_files
+		return []
+
+	def persistenceDirRecursion( self, mydir ):
+		for f in mydir['files']:
+			if f['type'] == 'dir':
+				self.persistenceDirRecursion( f )
+				f['type'] = 'dirinmeta'
+				f['files'] = []
+		path = self.makefile( mydir['namehash'], 'meta' )
+		f = open(path,'w')
+		f.write(json.dumps(mydir['files'],indent=2))
+		f.close()
+		
+
+
 
--- root.py
+++ root.py
@@ -2,8 +2,9 @@ import sys
 import fs45g
 
 fs = fs45g.fs45g( sys.argv[1], sys.argv[2] )
-key,obj = fs.prepareDir('/',0,0,'664')
-fs.persistenceDir(key,obj)
+obj = fs.prepareDir('',0,0,'664')
+fs.prepareDirInDir('home', 0, 0, '664', obj) 
+fs.persistenceDirRecursion(obj)
 
 
 
