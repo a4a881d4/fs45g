74ab95f1eeba0e5764401df46ae96d4fe0af334b
a4a881d4 <a4a881d4@163.com>
2015-07-10 23:58:04 +0800

    rewrite from s3fs

--- fs45g/fs45g.py
+++ fs45g/fs45g.py
@@ -1,123 +1,403 @@
 import tools
 import os
 import time
-import json
+import fuse
+import pickle
+import syslog
+import getopt
+import sys
+import errno
 
+from fuse import Fuse
+from keylayoutelement import KeyLayoutElement
+from fs45gcashe import fs45gCashe
+from fs45gstat import fs45gStat,fs45gROStat
+from fs45gfile import fs45gFile
+from metadata import MetaData
 
-class fs45g:
-	def __init__(self,root,cashe):
-		self.root = root
-		self.cashe = root
+class fs45g(Fuse):
+	def __init__(self, *args, **kw):
+		Fuse.__init__(self, *args, **kw)
+		self.writeback_time = 10.0
+		self.lazy_fsdata=True
+		self.cachedir="/tmp"
+		self.persistence = "persistence"
+		self.root = "root"
 
-	def makefile(self,hashkey,rc):
-		if rc == 'meta':
-			path = self.root
-		else:
-			path = self.cashe
-		path += '/' + hashkey[:2]
-		
-		if not os.path.isdir(path):
-			os.mkdir(path)
+	def setup(self):
+		preserve = False
+		fsname = tools.hashpath('')
+		fsname = self.root + '/' +fsname		
+		fp = open(fsname, "rb")
+		self.FSData = pickle.load(fp)
+		fp.close()
+		self.FSData.root.runtimeSetup()
 
-		filename = path + '/' + hashkey
-		
-		return filename
+		self.cache = fs45gCashe('root',self.cachedir,-1,preserve)
+		self.cache.syncTree(self.FSData.root)
+
+		return True
+
+	def updateFsData(self):
+		fsdata_copy = self.FSData.cloneForSave()
+		fsname = tools.hashpath('')
+		fsname = self.root + '/' +fsname		
+		fp = open(fsname,"w+b")
+		pickle.dump(fsdata_copy,fp,-1)
+		fp.close()
 	
-	def _createMeta( self, path, UID, GID, mode ):
-		my = dict()
-		my['path']=path
-		my['uid']=UID
-		my['gid']=GID
-		my['mode']=tools.fmode(mode)
-		my['ctime']=time.time()
-		my['mtime']=time.time()
-		my['atime']=time.time()
-		my['nlink'] = 0
-		my['size'] = 0	
-		return my
+	def getattr(self, path):
+		findnode = self.FSData.findNode(path)		
+		if findnode == None:
+			return -errno.ENOENT
+		uid = findnode.stat.st_uid
+		gid = findnode.stat.st_gid
+		
+		if (uid == -1):
+			uid = os.getuid()
+		if (gid == -1):
+			gid = os.getgid()
 
-	def prepareDir( self, path, UID, GID, mode ):
-		mydir = self._createMeta( path, UID, GID, mode )
-		fns = path.split('/')
-		if fns:
-			mydir['name'] = fns[-1]
+		return fs45gROStat(findnode.stat, uid, gid) 
+
+	def readdir(self, path, offset):
+		dirNode = self.FSData.findNode(path)
+		dirNames = [ '.', '..']
+		for node in dirNode.children:
+			dirNames.append(node.name)
+		for i in dirNames:
+			yield fuse.Direntry(i)
+		
+	def chmod ( self, path, mode ):
+		node = self.FSData.findNode(path)
+		if node == None:
+			return -errno.ENOENT
+
+		node.iolock.acquire()
+		node.stat.st_mode = node.stat.st_mode & ~(0777)
+		node.stat.st_mode = node.stat.st_mode | mode
+		node.iolock.release()
+		return 0
+
+	def chown ( self, path, uid, gid ):
+		node = self.FSData.findNode(path)
+		if node == None:
+			return -errno.ENOENT
+		node.iolock.acquire()
+		node.stat.st_uid = uid
+		node.stat.st_gid = gid
+		node.iolock.release()
+
+	def link ( self, targetPath, linkPath ):
+		nodetok = path.split('/')
+		lname = nodetok[len(nodetok)-1]
+		nodetok.remove(lname)
+		dir = ""
+		for i in nodetok:
+			if i != '':
+				dir = dir + "/" + i
+		if dir == "":
+			dir = "/"
+	
+		dirnode = self.FSData.findNode(dir)
+		if dirnode == None:
+			return -errno.ENOENT
+
+		tnode = self.FSData.findNode(targetPath)
+		if tnode == None:
+			return -errno.ENOENT
+
+		lnode = KeyLayoutElement(lname)
+		lnode.runtimeSetup()
+		ndnode.stat.st_mode = S_IFLNK | 0777
+		lnode.target_path = targetPath
+		dirnode.addChild(lnode)
+		return 0
+
+	def mkdir ( self, path, mode ):
+		nodetok = path.split('/')
+		newdir = nodetok[len(nodetok)-1]
+		nodetok.remove(newdir)
+		dir = ""
+		for i in nodetok:
+			if i != '':
+				dir = dir + "/" + i
+
+		if dir == "":
+			dir = "/"
+	
+		dirnode = self.FSData.findNode(dir)
+		if dirnode == None:
+			return -errno.ENOENT
+
+		ndnode = KeyLayoutElement(newdir)
+		ndnode.runtimeSetup()
+		ndnode.stat.st_mode = stat.S_IFDIR | mode
+		dirnode.addChild(ndnode)
+		return 0
+
+	def mknod ( self, path, mode, dev ):
+		node = self.FSData.findNode(path)
+		if node != None:
+			return -errno.EEXIST
+		nodetok = path.split('/')
+		dir=""
+		filename = nodetok[len(nodetok)-1]
+		nodetok.remove(filename)
+		for i in nodetok:
+			if i != '':
+				dir = dir + "/" + i
+		if dir == "":
+			dir = "/"
+
+		dirnode = self.FSData.findNode(dir)
+		file = KeyLayoutElement(filename)
+		file.runtimeSetup()
+		file.stat.st_mode = mode
+		dirnode.addChild(file)
+		node = self.FSData.findNode(path)
+		fd = file.open(self) 
+		fd.close()
+		return 0
+
+	def readlink ( self, path ):
+		node = self.FSData.findNode(path)
+		if node == None:
+			return -errno.ENOENT
+		return node.target_path
+
+	def rename ( self, oldPath, newPath ):
+		oldnode = self.FSData.findNode(oldPath)
+		if oldnode == None:
+			return -errno.ENOENT
+		newnode = self.FSData.findNode(newPath)
+		if newnode != None:
+			return -errno.EEXIST
+
+		newntok = newPath.split('/')
+		newname = newntok[len(newntok)-1]
+		newntok.remove(newname)
+		dir = ""
+		for i in newntok:
+			if i != '':
+				dir = dir + "/" + i
+		if dir == "":
+			dir = "/"
+
+		newdirnode = self.FSData.findNode(dir)
+		if newdirnode == None:
+			return -errno.ENOENT
+	
+		newnode = KeyLayoutElement(newname)
+		newnode.runtimeSetup()
+		newnode.stat = oldnode.stat
+
+		newdirnode.addChild(newnode)
+
+		newfile = newnode.open(self, os.O_RDWR|os.O_CREAT)
+		oldfile = oldnode.open(self, os.O_RDWR)
+
+		newnode.iolock.acquire()
+		oldnode.iolock.acquire()
+
+		shutil.copyfileobj(oldfile,newfile)
+	
+		newnode.dirty = True
+
+		newnode.iolock.release()
+		oldnode.iolock.release()
+
+		newnode.close(self)
+		oldnode.close(self)
+
+		oldnode.delete(self)
+
+		return 0	
+
+	def rmdir ( self, path ):
+		node = self.FSData.findNode(path)
+		if node == None:
+			return -errno.ENOENT
+		if ((node.stat.st_mode & stat.S_IFDIR) != stat.S_IFDIR):
+			return -errno.ENOTDIR
+		node.parent.removeChild(node,self)
+		return 0 
+
+	def access (self, path, mode):
+		mask = 0007
+		node = self.FSData.findNode(path)
+		if node == None:
+			return -errno.ENOENT
+		if node.stat.st_uid == -1:
+			uid = -1
 		else:
-			mydir['name'] = ''
-		mydir['namehash'] = tools.hashpath(path)
-		mydir['type'] = 'dir'
-		mydir['files'] = []
-		return mydir
+			uid = os.getuid()
 
-	def persistenceDir( self, mydir ):
-		path = self.makefile( mydir['namehash'], 'meta' )
-		f = open(path,'w')
-		f.write(json.dumps(mydir['files'],indent=2))
-		f.close()
-		mydir['type'] = 'dirinmeta'
-		mydir['files'] = []
+		if node.stat.st_gid == -1:
+			gid = -1
+		else:
+			gid = os.getgid()
 
-	def prepareFileInDir( self, fn, UID, GID, mode, mydir ):
-		myfile = self._createMeta( mydir['path']+'/'+fn, UID, GID, mode )
-		myfile['name'] = fn
-		myfile['namehash'] = tools.hashpath(myfile['path'])
-		myfile['type'] = 'file'
-		mydir['files'].append(myfile)
+		if node.stat.st_uid == uid:
+			return 0 
+		if node.stat.st_guid == gid:
+			return 0 
 
-	def prepareDirInDir( self, fn, UID, GID, mode, mydir ):
-		myfile = self._createMeta( mydir['path']+'/'+fn, UID, GID, mode )
-		myfile['name'] = fn
-		myfile['namehash'] = tools.hashpath(myfile['path'])
-		myfile['type'] = 'dir'
-		myfile['files'] = []
-		mydir['files'].append(myfile)
+		return -errno.EACCESS 
 
-	def loadDirByPathRecursively( self, path ):
-		metapath = self.makefile( tools.hashpath(path), 'meta' )
-		with open(metapath) as meta_file:    
-			mydir_files = json.load(meta_file)
-			for f in mydir_files:
-				if f['type'] == 'dirinmeta':
-					f['files'] = self.loadDirByPath(f['path'])
-					f['type'] = 'dir'
-			return mydir_files
-		return []
-
-	def loadDirByPath( self, path ):
-		metapath = self.makefile( tools.hashpath(path), 'meta' )
-		with open(metapath) as meta_file:    
-			mydir_files = json.load(meta_file)
-			return mydir_files
-		return []
-
-	def persistenceDirRecursively( self, mydir ):
-		for f in mydir['files']:
-			if f['type'] == 'dir':
-				self.persistenceDirRecursively( f )
-				f['type'] = 'dirinmeta'
-				f['files'] = []
-		path = self.makefile( mydir['namehash'], 'meta' )
-		f = open(path,'w')
-		f.write(json.dumps(mydir['files'],indent=2))
-		f.close()
-
-	def _syncDir( self, files ):
-		for f in files:
-			if f['type'] == 'dir':
-				self._syncDir( f['files'] )
-				metapath = self.makefile( tools.hashpath(f['path']), 'meta' )
-				if os.path.isfile(metapath):
-					os.remove(metapath)
-			elif f['type'] == 'dirinmeta':
-				load_files = self.loadDirByPath(f['path'])
-				self._syncDir( load_files )
 		
-	def syncDirRecursively( self, mydir ):
-		self._syncDir( mydir['files'] )
-		path = self.makefile( mydir['namehash'], 'meta' )
-		f = open(path,'w')
-		f.write(json.dumps(mydir['files'],indent=2))
-		f.close()	
+	def getxattr(self, path, name, size):
+		node = self.FSData.findNode(path)
+		if node == None:
+			return -errno.ENOENT
+
+		try:
+			attr = node.xattrs[name]
+		except KeyError:
+			return -errno.ENODATA
+		if size == 0:
+			return len(attr)
+		return attr
+
+	def setxattr(self, path, name, value):
+		node = self.FSData.findNode(path)
+		if node == None:
+			return -errno.ENOENT
+
+		node.xattrs[name] = value
+
+	def statfs ( self ):
+		st = fuse.StatVfs()
+		st.f_bsize = 1024 
+		st.f_frsize = 0x7fffffff
+		st.f_blocks = 0x7fffffff
+		st.f_bfree =  0x7fffffff
+		st.f_bavail = 0x7fffffff
+		st.f_files =  0x7fffffff
+		st.f_ffree =  0x7fffffff
+		st.f_namelen = 255 
+		return st 
+		
+	def symlink ( self, targetPath, linkPath ):
+		nodetok = linkPath.split('/')
+		lname = nodetok[len(nodetok)-1]
+		nodetok.remove(lname)
+		dir = ""
+		for i in nodetok:
+			if i != '':
+				dir = dir + "/" + i
+
+		if dir == "":
+			dir = "/"
+	
+		dirnode = self.FSData.findNode(dir)
+		if dirnode == None:
+			return -errno.ENOENT
+
+		lnode = KeyLayoutElement(lname)
+		lnode.runtimeSetup()
+		lnode.stat.st_mode = stat.S_IFLNK | 0777
+		lnode.target_path = targetPath
+		dirnode.addChild(lnode)
+
+	def unlink ( self, path ):
+		node = self.FSData.findNode(path)
+		if node == None:
+			return -errno.ENOENT
+		node.delete(self)
+		return 0
+
+	def utime(self, path, times):
+		return 0
+
+	def utimens(self, path, ts_acc, ts_mod):
+		return 0
+
+	def main(self, *a, **kw):
+		self.file_class = fs45gFile
+		fs45gFile.myFS = self
+		return Fuse.main(self, *a, **kw)
+
+	def shutdown(self):
+		syslog.syslog(syslog.LOG_INFO, "Exiting, Cleaning up FS")
+
+		syslog.syslog(syslog.LOG_INFO, "Verifying writeback")
+		self.FSData.root.writebackAll(self)
+
+		syslog.syslog(syslog.LOG_INFO, "Waiting on all threads to finish")
+		self.FSData.root.completeAllIo(True, True)
+
+		self.cache.shutdown()
+
+		if self.lazy_fsdata == True:
+			syslog.syslog(syslog.LOG_INFO, "Updating the Filesystem Metadata")
+			self.updateFsData()	
+	
+def fs45gEmpty( root="root" ):
+	fuse.fuse_python_api=(0, 2)
+	fs = fs45g()
+	fs.root = root
+	fs.FSData = MetaData()
+	fs.updateFsData()
 		
 
+def fs45g_cleanup(filesystem):
+	filesystem.shutdown()
+		
+def main():
+	fuse.fuse_python_api=(0, 2)
+	fs = fs45g()
+	try:
+		opts, args = getopt.getopt(sys.argv[1:], "r:p:")
+	except getopt.GetoptError:
+		fs_usage()
+		return 1
+	for o, a in opts:
+		if o == "-r":
+			fs.root = a
+		if o == "-p":
+			fs.persistence = a
+	syslog.openlog(ident="fs45g", facility=syslog.LOG_USER)
+	fs.parse(values=fs,errex=1)
+	fs.flags = 0
+	fs.multithreaded = True 
+	if fs.setup() == False:
+		syslog.closelog()
+		sys.exit(1)
+	fs.main()	
+	fs45g_cleanup(fs)
+	syslog.closelog()
+
+def fs_usage():
+	print "./fs45g -C -hc <root> <persistence> <mountpoint>"
+	print "-C enter command mode"
+	print "-h This help menu"
+	
+def handle_command_mode():
+	try:
+		opts, args = getopt.getopt(sys.argv[2:], "hc")
+	except getopt.GetoptError:
+		fs_usage()
+		return 1
+	for o, a in opts:
+		if o == "-h":
+			fs_usage()
+			return 0
+		if o == "-c":
+			fs45gEmpty(sys.argv[3])
+			return 0
+	fs_usage()
+	return 0
+
+
+if __name__ == '__main__':
+	if len(sys.argv) == 1 or sys.argv[1] == "-h":
+		fs_usage()
+		sys.exit(0)
+	if sys.argv[1] == "-C":
+		rc = handle_command_mode()
+		sys.exit(rc)
+	else:
+		main()
 
 
--- fs45g/tools.py
+++ fs45g/tools.py
@@ -19,4 +19,18 @@ def hashpath( path ):
 	dir_hash = base64.urlsafe_b64encode(sh.digest())
 	return dir_hash
 
+def flag2mode(flags):
+	md = {os.O_RDONLY: 'r', os.O_WRONLY: 'w', os.O_RDWR: 'w+'}
+	m = md[flags & (os.O_RDONLY | os.O_WRONLY | os.O_RDWR)]
 
+	if flags | os.O_APPEND:
+		m = m.replace('w', 'a', 1)
+
+	return m
+
+def makefilename(self,hashkey,dir):
+	path = dir + '/' + hashkey[:2]
+	if not os.path.isdir(path):
+		os.mkdir(path)
+	filename = path + '/' + hashkey
+	return filename
